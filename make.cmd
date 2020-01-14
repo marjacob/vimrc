c6ba55926dffdb626c9fdc5e6283b9acf434b0f5
marjacob <marjacob@users.noreply.github.com>
2020-01-14 13:28:29 +0100

    Add Makefile "simulator" for Windows

---
+++ make.cmd
@@ -0,0 +1,49 @@
+@echo off
+
+set bundle=bundle.tgz
+set zip=%~dp0bin\win32\7za.exe
+
+if [%1] == [] (
+	call :create_archive
+	exit /b 0
+)
+
+if /i "%1" == "bundle" (
+	call :create_archive
+)
+
+if /i "%1" == "clean" (
+	call :delete_archive
+)
+
+if /i "%1" == "init" (
+	git submodule update --init --recursive
+)
+
+if /i "%1" == "update" (
+	git submodule foreach git pull origin master
+)
+
+exit /b 0
+
+:create_archive
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+call :delete_archive
+
+%zip% a -ttar -so -an^
+	Makefile^
+	README.md^
+	vimrc^
+	.gitignore^
+	.gitmodules^
+	pack | %zip% a -si "%bundle%"
+exit /b 0
+
+:delete_archive
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+if exist "%bundle%" (
+	del "%bundle%"
+)
+
+exit /b 0
+
